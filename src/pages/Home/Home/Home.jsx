d2791ab08bdf1838c4967264cd621bfdf35a7999
Apur0y <apuroy2785@gmail.com>
2024-12-31 14:06:22 +0600

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            I am in home
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
