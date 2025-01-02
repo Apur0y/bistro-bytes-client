ee5e5a6d111b098c73f72d3ab92ccf8ba85b3897
Apur0y <apuroy2785@gmail.com>
2025-01-02 22:14:48 +0600

    section title set

---
+++ src/component/sectionTitle/sectionTitle.jsx
@@ -0,0 +1,13 @@
+
+
+const titleName = ({heading,subHeading}) => {
+    return (
+        <div>
+            <p>{subHeading}</p>
+            <h3>{heading}</h3>
+            <p>How to work</p>
+        </div>
+    );
+};
+
+export default titleName;
---
+++ src/pages/Home/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div className='text-5xl'>
+            Hope rwe do
+        </div>
+    );
+};
+
+export default User;
