108efddded60ce42dda9efbe1abae2a39123b0ab
Apur0y <apuroy2785@gmail.com>
2025-01-04 22:11:51 +0600

    cover page

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-import {
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
 
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './Routes/Routes';
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <div className='max-w-screen-xl mx-auto'>
-  <RouterProvider router={router} />
-  </div>
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <div className="max-w-screen-xl mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
+  </StrictMode>
+);
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Bytes | Home</title>
+            </Helmet>
           
             <Banner></Banner>
             <Category></Category>
