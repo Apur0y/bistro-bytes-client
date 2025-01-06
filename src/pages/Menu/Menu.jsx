45541d069d5e11aa8ec39ed0a4f41ad88ca11580
Apur0y <apuroy2785@gmail.com>
2025-01-06 22:47:37 +0600

    All category insert in Menu page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Cover from '../Shared/Cover/Cover';
 import { Helmet } from 'react-helmet-async';
 import menuImg from "../../assets/menu/banner3.jpg"
+import saladImg from "../../assets/menu/salad-bg.jpg"
+import dessertImg from "../../assets/menu/dessert-bg.jpeg"
+import soupImg from "../../assets/menu/soup-bg.jpg"
+import pizzaImg from "../../assets/menu/pizza-bg.jpg"
 import useMenu from '../../hook/useMenu';
 import OurMenu from '../Home/OurMenu/OurMenu';
 import TitleName from '../../component/SectionTitle/TitleName';
@@ -15,7 +19,7 @@ const Menu = () => {
     const pizza = menu.filter(item=>item.category=="pizza")
     const dessert = menu.filter(item=>item.category=="dessert")
     const offered = menu.filter(item=>item.category=="offered")
-    console.log(offered);
+    console.log(offered,salad,soup,pizza);
 
     return (
         <div>
@@ -25,7 +29,10 @@ const Menu = () => {
             <Cover img={menuImg} title={"Our Menu"}></Cover>
             <TitleName heading={"Today's Offer"} subHeading={"Don't Miss"}></TitleName>
             <MenuCategory items={offered}></MenuCategory>
-            <MenuCategory items={salad} coverImg={menuImg} title={"Salad"}></MenuCategory>
+            <MenuCategory items={salad} coverImg={saladImg} title={"Salad"}></MenuCategory>
+            <MenuCategory items={soup} coverImg={soupImg} title={"soup"}></MenuCategory>
+            <MenuCategory items={pizza} coverImg={pizzaImg} title={"pizza"}></MenuCategory>
+            <MenuCategory items={dessert} coverImg={dessertImg} title={"dessert"}></MenuCategory>
            
         </div>
     );
